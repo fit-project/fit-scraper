d370d1f4530bd7545f402871ca094322bfa32219
Fabio Zito <zitelog@gmail.com>
2025-07-18 11:44:51 +0200

    version 0.1.0

---
+++ fit_scraper/scraper.py
@@ -0,0 +1,296 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+######
+# -----
+# Copyright (c) 2023 FIT-Project
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# -----
+######
+
+import os
+import re
+import stat
+
+from fit_acquisition.acquisition import Acquisition, AcquisitionStatus
+from fit_acquisition.tasks.tasks_info import TasksInfo
+from fit_cases.utils import show_case_info_dialog
+from fit_cases.view.case_form_dialog import CaseFormDialog
+from fit_common.core.utils import resolve_path
+from fit_common.gui.error import Error
+from fit_common.gui.utils import show_finish_acquisition_dialog
+from fit_configurations.controller.tabs.general.general import GeneralController
+from fit_configurations.utils import show_configuration_dialog
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtGui import QMovie
+
+from fit_scraper.lang import load_scraper_translations
+
+
+class Scraper(QtWidgets.QMainWindow):
+
+    def __init__(self, logger, acquisition_type, packages, wizard=None):
+        super().__init__()
+
+        self.__acquisition_status = AcquisitionStatus.UNSTARTED
+        self.__acquisition_type = acquisition_type
+        self.__has_valid_case = True
+        self.__case_info = None
+        self.__acquisition_directory = None
+        self.__tasks_info = None
+
+        self.__wizard = wizard
+
+        self.scraper_translations = load_scraper_translations()
+
+        if self.__wizard is not None:
+            self.__case_info = self.__wizard.case_info
+        else:
+            dialog = CaseFormDialog()
+            dialog.ui.save_button.setText("Start")
+            result = dialog.exec()
+            if result == QtWidgets.QDialog.Accepted:
+                self.__case_info = dialog.get_case_info()
+            else:
+                error_dlg = Error(
+                    QtWidgets.QMessageBox.Icon.Critical,
+                    self.scraper_translations["NO_CASE_SELECTED_TITLE"],
+                    self.scraper_translations["NO_CASE_SELECTED_MESSAGE"],
+                    "",
+                )
+                error_dlg.exec()
+                self.__has_valid_case = False
+                return
+
+        self.__acquisition = Acquisition(logger=logger, packages=packages)
+        self.__acquisition.start_tasks_finished.connect(self.on_start_tasks_finished)
+        self.__acquisition.stop_tasks_finished.connect(self.on_stop_tasks_finished)
+        self.__acquisition.post_acquisition_finished.connect(
+            self.on_post_acquisition_finished
+        )
+
+    @property
+    def acquisition_status(self):
+        return self.__acquisition_status
+
+    @acquisition_status.setter
+    def acquisition_status(self, value):
+        self.__acquisition_status = value
+
+    @property
+    def acquisition_type(self):
+        return self.__acquisition_type
+
+    @acquisition_type.setter
+    def acquisition_type(self, value):
+        self.__acquisition_type = value
+
+    @property
+    def has_valid_case(self):
+        return self.__has_valid_case
+
+    @property
+    def acquisition(self):
+        return self.__acquisition
+
+    @property
+    def case_info(self):
+        return self.__case_info
+
+    def create_acquisition_directory(self) -> bool:
+        try:
+            # Folder Cases
+            cases_folder = os.path.expanduser(
+                GeneralController().configuration["cases_folder_path"]
+            )
+            if not os.path.exists(cases_folder):
+                os.makedirs(cases_folder)
+                os.chmod(cases_folder, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+
+            # Folder Case
+            case_folder = os.path.join(cases_folder, self.__case_info["name"])
+            if not os.path.exists(case_folder):
+                os.makedirs(case_folder)
+                os.chmod(case_folder, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+
+            # Folder Type
+            acquisition_type_folder = os.path.join(case_folder, self.__acquisition_type)
+            if not os.path.exists(acquisition_type_folder):
+                os.makedirs(acquisition_type_folder)
+                os.chmod(
+                    acquisition_type_folder, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO
+                )
+
+            self.acquisition_directory = os.path.join(
+                acquisition_type_folder, "acquisition_1"
+            )
+
+            if os.path.isdir(self.acquisition_directory):
+                acquisition_directories = [
+                    d
+                    for d in os.listdir(acquisition_type_folder)
+                    if os.path.isdir(os.path.join(acquisition_type_folder, d))
+                ]
+
+                acquisition_directories = list(
+                    filter(
+                        lambda item: bool(re.search(r"^acquisition_(\d+)$", item)),
+                        acquisition_directories,
+                    )
+                )
+
+                index = max(
+                    [
+                        int("".join(filter(str.isdigit, item)))
+                        for item in acquisition_directories
+                    ],
+                    default=0,
+                )
+
+                self.acquisition_directory = os.path.join(
+                    acquisition_type_folder, f"acquisition_{index + 1}"
+                )
+
+            os.makedirs(self.acquisition_directory, exist_ok=True)
+            return True
+
+        except Exception as e:
+            error = Error(
+                QtWidgets.QMessageBox.Icon.Critical,
+                self.scraper_translations["CREATE_DIRECTORY_ERROR_TITLE"],
+                self.scraper_translations["CREATE_ACQUISITION_DIRECTORY_ERROR_MESSAGE"],
+                str(e),
+            )
+            error.exec()
+            self.acquisition_directory = None
+            return False
+
+    def create_acquisition_subdirectory(self, sub_path: str) -> bool:
+        try:
+            if not self.acquisition_directory:
+                raise ValueError(
+                    self.scraper_translations["ACQUISITION_DIRECTORY_DOES_NOT_EXIST"]
+                )
+
+            full_path = os.path.join(self.acquisition_directory, sub_path)
+            os.makedirs(full_path, exist_ok=True)
+            os.chmod(full_path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+            return True
+
+        except Exception as e:
+            error = Error(
+                QtWidgets.QMessageBox.Icon.Critical,
+                self.scraper_translations["CREATE_DIRECTORY_ERROR_TITLE"],
+                self.scraper_translations[
+                    "CREATE_ACQUISITION_SUBDIRECTORY_ERROR_MESSAGE"
+                ].format(sub_path),
+                str(e),
+            )
+            error.exec()
+            return False
+
+    def execute_start_tasks_flow(self):
+        self._reset_acquisition_indicators(True)
+        self.__acquisition.load_tasks()
+        self.__init_execution_overlay()
+        self.__spinner_overlay.show()
+        self.__movie_spinner.start()
+        loop = QtCore.QEventLoop()
+        QtCore.QTimer.singleShot(1000, loop.quit)
+        loop.exec()
+        self.__acquisition.run_start_tasks()
+
+    def on_start_tasks_finished(self):
+        loop = QtCore.QEventLoop()
+        QtCore.QTimer.singleShot(2000, loop.quit)
+        loop.exec()
+        self.__movie_spinner.stop()
+        self.__spinner_overlay.hide()
+        self.__acquisition.progress_bar.setValue(100)
+        self._reset_acquisition_indicators(False)
+
+    def execute_stop_tasks_flow(self):
+        self._reset_acquisition_indicators(True)
+        self.__tasks_info.show()
+        loop = QtCore.QEventLoop()
+        QtCore.QTimer.singleShot(1000, loop.quit)
+        loop.exec()
+        self.__acquisition.run_stop_tasks()
+
+    def on_stop_tasks_finished(self):
+        self.execute_post_acquisition_tasks_flow()
+        self._reset_acquisition_indicators(False)
+
+    def execute_post_acquisition_tasks_flow(self):
+        self._reset_acquisition_indicators(True)
+        self.__acquisition.start_post_acquisition()
+
+    def on_post_acquisition_finished(self):
+        QtCore.QTimer.singleShot(1000, self.__tasks_info.close)
+        self.__acquisition.log_end_message()
+        self.__acquisition.set_completed_progress_bar()
+        self.__acquisition.unload_tasks()
+        self._reset_acquisition_indicators(False)
+
+    def on_resize(self, event):
+        self.__spinner_overlay.setGeometry(self.rect())
+        self.__tasks_info.setGeometry(self.rect())
+        super().resizeEvent(event)
+
+    def finish_acquisition(self):
+        show_finish_acquisition_dialog(self.__acquisition_directory)
+
+    def configuration_dialog(self):
+        show_configuration_dialog()
+
+    def show_case_info(self):
+        show_case_info_dialog(self.__case_info)
+
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPosition().toPoint()
+
+    def move_window(self, event):
+        if event.buttons() == QtCore.Qt.MouseButton.LeftButton:
+            self.move(self.pos() + event.globalPosition().toPoint() - self.dragPos)
+            self.dragPos = event.globalPosition().toPoint()
+            event.accept()
+
+    def _reset_acquisition_indicators(self, visible: bool):
+        self.__acquisition.progress_bar_visible = visible
+        self.__acquisition.status_bar_visible = visible
+        self.__acquisition.reset_progress_bar
+        self.__acquisition.reset_status_bar
+
+    def __init_execution_overlay(self):
+
+        self.resizeEvent = self.on_resize
+
+        self.__tasks_info = TasksInfo(parent=self)
+        self.__tasks_info.setWindowFlags(QtCore.Qt.WindowType.Widget)
+        self.__tasks_info.setAutoFillBackground(True)
+        self.__tasks_info.setGeometry(self.rect())
+        self.__tasks_info.hide()
+        self.__tasks_info.raise_()
+
+        self.__spinner_overlay = QtWidgets.QLabel(self)
+        self.__spinner_overlay.setAlignment(QtCore.Qt.AlignCenter)
+        self.__spinner_overlay.setStyleSheet(
+            "background-color: rgba(255, 255, 255, 180);"
+        )
+
+        self.__spinner_overlay.setGeometry(self.rect())
+        self.__spinner_overlay.hide()
+
+        self.__movie_spinner = QMovie(resolve_path("spinner.gif"))
+        self.__spinner_overlay.setMovie(self.__movie_spinner)
+
+    def __back_to_wizard(self):
+        self.deleteLater()
+        self.__wizard.reload_case_info()
+        self.__wizard.show()
+
+    def closeEvent(self, event):
+        if self.__wizard is not None:
+            event.ignore()
+            self.__back_to_wizard()
+        else:
+            event.accept()
---
+++ main.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+######
+# -----
+# Copyright (c) 2023 FIT-Project
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# -----
+######
+
+import logging
+import os
+import sys
+
+from fit_acquisition.class_names import *
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtWidgets import QApplication
+
+from fit_scraper.scraper import Scraper
+
+
+class TestScraper(Scraper):
+    def __init__(self, wizard=None):
+        logger = logging.getLogger("view.scrapers.web.web")
+        packages = []
+        super().__init__(logger, "web", packages, wizard)
+
+        self.acquisition.start_tasks = [SCREENRECORDER, PACKETCAPTURE]
+        self.acquisition.stop_tasks = [
+            WHOIS,
+            NSLOOKUP,
+            HEADERS,
+            SSLKEYLOG,
+            SSLCERTIFICATE,
+            TRACEROUTE,
+            SCREENRECORDER,
+            PACKETCAPTURE,
+        ]
+        self.__init_execution_overlay()
+
+    def __init_execution_overlay(self):
+        self.setWindowTitle("Test Scraper Window")
+        self.setMinimumSize(800, 600)
+
+        label = QtWidgets.QLabel("Questa è una finestra base di test.", self)
+        label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        central_widget = QtWidgets.QWidget()
+        layout = QtWidgets.QVBoxLayout(central_widget)
+        layout.addWidget(label)
+
+        self.acquisition.progress_bar = QtWidgets.QProgressBar()
+        self.acquisition.status_bar = QtWidgets.QLabel()
+
+        self.start_btn = QtWidgets.QPushButton("Start")
+        self.stop_btn = QtWidgets.QPushButton("Stop")
+
+        layout.addWidget(self.acquisition.progress_bar)
+        layout.addWidget(self.acquisition.status_bar)
+        layout.addWidget(self.start_btn)
+        layout.addWidget(self.stop_btn)
+
+        self.start_btn.clicked.connect(self.execute_start_tasks_flow)
+        self.stop_btn.clicked.connect(self.execute_stop_tasks_flow)
+
+        self.setCentralWidget(central_widget)
+
+        self._reset_acquisition_indicators(False)
+
+    def execute_start_tasks_flow(self):
+        if self.create_acquisition_directory():
+            if self.create_acquisition_subdirectory("screenshot"):
+                acquisition_page = os.path.join(
+                    self.acquisition_directory, "acquisition_page"
+                )
+                os.makedirs(acquisition_page, exist_ok=True)
+                self.acquisition.options = {
+                    "acquisition_directory": self.acquisition_directory,
+                    "screenshot_directory": os.path.join(
+                        self.acquisition_directory, "screenshot"
+                    ),
+                    "current_widget": self,
+                    "window_pos": self.centralWidget().pos(),
+                    "url": "http://google.it",
+                    "type": "web",
+                    "case_info": self.case_info,
+                    "pdf_filename": "acquisition_report.pdf",
+                }
+
+                return super().execute_start_tasks_flow()
+
+    def execute_stop_tasks_flow(self):
+        return super().execute_stop_tasks_flow()
+
+    def on_start_tasks_finished(self):
+        print("finito di eseguire tutti i task della lista di Acquisition start_tasks")
+        return super().on_start_tasks_finished()
+
+    def on_stop_tasks_finished(self):
+        self.acquisition.start_post_acquisition()
+        print("finito di eseguire tutti i task della lista di Acquisition  stop_tasks")
+
+    def on_post_acquisition_finished(self):
+        print("finito di eseguire tutti i task della lista di Acquisition post_tasks")
+        return super().on_post_acquisition_finished()
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = TestScraper()
+
+    if window.has_valid_case:
+        window.show()
+        sys.exit(app.exec())
+    else:
+        print("Utente ha annullato il form del caso. Niente da mostrare.")
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
